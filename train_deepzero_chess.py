34cf8caf0d805c8b628eb091acb5a5884edb5a8b
Qwest1204 <myPCwithUbuntu@gmail.com>
2025-12-16 10:05:08 +0300

    add train chess

---
+++ train_deepzero_chess.py
@@ -0,0 +1,29 @@
+from games.checkers import Checkers
+from models.resnet import ResNet
+from models.deepzero import DeepZeroParallel
+import torch
+
+game = Checkers()
+
+device = torch.device("cuda")
+
+model = ResNet(game, 32, 512, device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
+
+player = 1
+
+args = {
+    'C': 1.5,
+    'num_searches': 2000,
+    'num_iterations': 60,
+    'num_parallel_games': 128,
+    'batch_size': 128,
+    'num_selfPlay_iterations': 2000,
+    'num_epochs': 10,
+    'temperature': 1.25,
+    'dirichlet_epsilon': 0.25,
+    'dirichlet_alpha': 0.3
+}
+
+deepzero = DeepZeroParallel(model, optimizer, game, args)
+deepzero.learn()
